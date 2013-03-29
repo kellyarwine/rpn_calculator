924d4cee74cd0f12415205671307df7c582c2f3a
Kelly Steensma <kellysteensma@gmail.com>
2013-03-29 10:11:22 -0500

    Working version but needs refactoring.

--- lib/postfix.rb
+++ lib/postfix.rb
@@ -1,35 +1,55 @@
 class Postfix
   OPERATION_SYMBOLS = ["*","/","-","+"]
 
-  def calculate(n)
-    convert_string_to_array(n)
+  def calculate(equation)
+    convert_string_to_array(equation)
 
-    if first_element_math_symbol?
-      operation
-    else
-      @calculate_array.first.to_i
+    while @equation_array.find { |element| element =~ /[\+\-\*\/]/ }
+      calculate_running_total
     end
+
+    @equation_array.first.to_i
   end
 
   def convert_string_to_array(string)
-    @calculate_array = string.split
+    @equation_array = string.split
   end
 
-  def first_element_math_symbol?
-    OPERATION_SYMBOLS.include? @calculate_array.last
-  end
-
-  def operation
-    if @calculate_array.last == "+"
-      @calculate_array[0].to_i + @calculate_array[1].to_i
-    elsif @calculate_array.last == "-"
-      @calculate_array[0].to_i - @calculate_array[1].to_i
-    elsif @calculate_array.last == "*"
-
-      @calculate_array[0].to_i * @calculate_array[1].to_i
-    elsif @calculate_array.last == "/"
-      @calculate_array[0].to_i / @calculate_array[1].to_i
+  def calculate_running_total
+    @equation_array.each_with_index do |element,index|
+      if OPERATION_SYMBOLS.include? element
+        operation(element,index)
+        delete_evaluated_elements(index)
+        return
+      end
     end
+  end
+
+  def operation(operator,operator_index)
+      operands = [first_operand(operator_index),second_operand(operator_index)]
+      @result = operands.reduce(operator.to_sym)
+  end
+
+  def first_operand_index(operator_index)
+    operator_index-2
+  end
+
+  def first_operand(operator_index)
+    @equation_array[first_operand_index(operator_index)].to_i
+  end
+
+  def second_operand_index(operator_index)
+    operator_index-1
+  end
+
+  def second_operand(operator_index)
+    @equation_array[second_operand_index(operator_index)].to_i
+  end
+
+  def delete_evaluated_elements(index)
+    @equation_array[index] = @result.to_s
+    @equation_array.delete_at(second_operand_index(index))
+    @equation_array.delete_at(first_operand_index(index))
   end
 
 end
